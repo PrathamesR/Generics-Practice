4d4d55630ec32e50e9fbce2e0be9ada308e190fc
Prathamesh Rajput <rajputprathamesh1998@gmail.com>
2020-10-20 09:54:46 +0530

    [Prathamesh Rajput] Add. Completed Use Case 2

--- Program.cs
+++ Program.cs
@@ -6,37 +6,36 @@ using System.Threading.Tasks;
 
 namespace GenericsPractice
 {
+
+
+    public class PrintArray<dataType>
+    {
+        private dataType[] arr;
+
+        public PrintArray(dataType[] arr)
+        {
+            this.arr = arr;
+        }
+
+        public void PrintArr()
+        {
+            Console.WriteLine();
+            foreach (dataType elem in arr)
+                Console.Write(elem+", ");
+        }
+    }
+
     class Program
     {
-        static void printArray(int[] arr)
-        {
-            foreach (var element in arr)
-                Console.Write(element + " ");
-        }
-
-        static void printArray(double[] arr)
-        {
-            Console.WriteLine();
-            foreach (var element in arr)
-                Console.Write(element + " ");
-        }
-
-        static void printArray(char[] arr)
-        {
-            Console.WriteLine();
-            foreach (var element in arr)
-                Console.Write(element + " ");
-        }
-
         static void Main(string[] args)
         {
             int[] intArray = { 1, 23, 45, 21, 48, 4 };
             double[] doubleArray = { 1, 2.3, 4.5, 2.1, 4.8, 4.9 };
             char[] charArray = { 'A','b','c' };
 
-            printArray(intArray);
-            printArray(doubleArray);
-            printArray(charArray);
+            new PrintArray<int>(intArray).PrintArr();
+            new PrintArray<double>(doubleArray).PrintArr();
+            new PrintArray<char>(charArray).PrintArr();
 
             Console.Read();
         }        
